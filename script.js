cd5ded175a286eb6f5493adf9b90c61197c9e7f8
MidnightMarathon <mariobiendarra21@gmail.com>
2025-07-06 17:28:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/qr-code-styling@1.5.0/lib/qr-code-styling.js"></script>
+<script src="script.js" defer></script>
+
 window.addEventListener("load", () => {
   const qrCode = new QRCodeStyling({
     width: 1080,
